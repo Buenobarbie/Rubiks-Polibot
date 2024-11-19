6a76da0666b4e5f5716564e40580d4229a5c9021
Buenobarbie <barbarabueno140104@gmail.com>
2024-11-19 09:52:09 -0300

    🐛 Fix missing ;

--- src/componentes/servo_matcher.v
+++ src/componentes/servo_matcher.v
@@ -7,6 +7,6 @@ module servo_matcher(
 
     assign peteleco = ~move[2] && ~move[1] && move[0];
     assign tampa = ~move[2] && move[1] && ~move[0];
-    assign base = move[2]
+    assign base = move[2];
 
 endmodule
